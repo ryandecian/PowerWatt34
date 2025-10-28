0d719e56df1aa0ddc7ef85492535e47912d11d25
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-28 11:07:20 +0100

    Mise a jour du front

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -15,7 +15,7 @@ const router: Router_Type[] = [
     /* Exemple d’accès à la première route : {router[0].path} */
     {
         path: "/",
-        element: <h1>Accueil PowerWatt34 sous React</h1>,
+        element: <h1>Accueil PowerWatt34</h1>,
     },
 ];
 
