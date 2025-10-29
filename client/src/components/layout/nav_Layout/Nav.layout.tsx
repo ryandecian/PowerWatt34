273f515c9a67b07406cefbac40af2993d37260c8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-29 13:39:06 +0100

    Création de Nav_Layout

--- client/src/components/layout/nav_Layout/Nav.layout.tsx
+++ client/src/components/layout/nav_Layout/Nav.layout.tsx
@@ -1,7 +1,21 @@
-
+/* Import des Hooks */
+import useMediaQueriesWidth_rem from "../../../hook/useMediaQueriesWidth_rem.utils.hook";
 
 function Nav_Layout() {
-    return <div>Nav Layout</div>;
+    const viewportWidth: number = useMediaQueriesWidth_rem();
+
+    return (
+        <>
+            {/* MediaQuery maison 800px */}
+            {viewportWidth < 50 && (
+                <p>Nav Mobile</p>
+            )}
+            {/* MediaQuery maison 800px */}
+            {viewportWidth >= 50 && (
+                <p>Nav PC</p>
+            )}
+        </>
+    )
 }
 
 export default Nav_Layout;
