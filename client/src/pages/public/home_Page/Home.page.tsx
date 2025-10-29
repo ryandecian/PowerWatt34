6c291e8e4db4f1b21ad9a919d30f76b357227f4b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-29 13:33:19 +0100

    Mise a jour de HomePage

--- client/src/pages/public/home_Page/Home.page.tsx
+++ client/src/pages/public/home_Page/Home.page.tsx
@@ -1,11 +1,21 @@
+/* Import des modules CSS */
+import style from "../../stylePage.module.css";
+
 /* Import des Components */
-import Nav_Layout from "../../../components/layout/nav_Layout/nav.layout";
+import Nav_Layout from "../../../components/layout/nav_Layout/Nav.layout";
 
 function HomePage() {
     return (
         <section className={`Home_Page ${style.Page}`}>
-            <header id="top"></header>
-            <Nav_Layout />
+            <header id="top" className={style.Header}>
+                <Nav_Layout />
+            </header>
+            <main className={style.Main}>
+                <p>main</p>
+            </main>
+            <footer className={style.Footer}>
+                <p>footer</p>
+            </footer>
         </section>
     );
 }
