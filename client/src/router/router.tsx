20be24b47413b0c64b189ab6ccdcbaa0bbbc21e4
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-06 15:19:48 +0100

    mise a jour du router

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -24,6 +24,26 @@ const router: Router_Type[] = [
         path: "/contact",
         element: <Contact_Page />,
     },
+    /* {router[1].path} */
+    {
+        path: "/contact",
+        element: <Contact_Page />,
+    },
+    /* {router[2].path} */
+    {
+        path: "/user/compte",
+        element: <h1>compte utilisateur</h1>,
+    },
+    /* {router[3].path} */
+    {
+        path: "/admin/compte",
+        element: <h1>compte admin</h1>,
+    },
+    /* {router[4].path} */
+    {
+        path: "/produits/trottinette",
+        element: <h1>produit trottinette</h1>,
+    },
 ];
 
 export default router;
