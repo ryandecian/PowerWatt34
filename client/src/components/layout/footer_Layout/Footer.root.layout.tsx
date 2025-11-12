e4dcab9b6998819d0a5c37ff37719bec6ad01ccb
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-12 10:12:15 +0100

    Initialisation du Footer

---
+++ client/src/components/layout/footer_Layout/Footer.root.layout.tsx
@@ -0,0 +1,11 @@
+/* Import des Components */
+
+function Footer_root_layout() {
+    return (
+        <footer>
+            <p>© 2024 MonSite. Tous droits réservés.</p>
+        </footer>
+    )
+}
+
+export default Footer_root_layout;
