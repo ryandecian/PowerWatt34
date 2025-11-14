88c96a6df6a367dcb3a78c86e14b0ed4b6719a56
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-14 17:42:54 +0100

    Création des types pour les card de produits de trottinette

--- client/src/components/public/trottinette/brands/dualtron/dualtron.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/dualtron.data.ts
@@ -1,8 +1,18 @@
-
-
+/* Import des Types */
+import type { TrottinetteBrand_data_Type } from "../../../../../types/produits/trottinetteBrands.data.type";
 
 function dualtron_Data() {
-    const tab = []
+    const tab: TrottinetteBrand_data_Type = [
+        {
+            reactKey: 1,
+            image: "",
+            path: "#",
+            model: "Dualtron X LTD",
+            battery: "84V 60Ah",
+            power: "13300W",
+            range: "200 km",
+        }
+    ]
 
     return (tab);
 }
