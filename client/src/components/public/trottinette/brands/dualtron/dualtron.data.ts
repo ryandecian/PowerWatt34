06a74cd8dd1a8e27494f3699c44704f3f9bb8925
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-15 13:12:30 +0100

    Ajout d'un nouveau produit, la dualtron storm

--- client/src/components/public/trottinette/brands/dualtron/dualtron.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/dualtron.data.ts
@@ -11,8 +11,18 @@ function dualtron_Data() {
             price: "5999 €",
             battery: "84V 60Ah", 
             power: "13300W",
-            range: "200 km",
-        }
+            range: "170 km",
+        },
+        {
+            reactKey: 2,
+            image: "/images/produits/trottinette/dualtron/dualtron-storm-ltd/dualtron-storm-ltd-n01-powerwatt34.jpg",
+            path: "#",
+            model: "Dualtron Storm LTD",
+            price: "4299 €",
+            battery: "84V 45Ah", 
+            power: "11500W",
+            range: "180 km",
+        },
     ]
 
     return (tab);
