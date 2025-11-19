1d5513422f008f6ba49a56e3dc88846f5a1b3cd3
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-19 16:25:20 +0100

    Ajout d"une police d'écriture

--- client/src/components/public/mentions-legales/MentionsLegales.root.tsx
+++ client/src/components/public/mentions-legales/MentionsLegales.root.tsx
@@ -1,8 +1,20 @@
-
+/* Import des modules CSS */
+import style from "../../styleRootComponent.module.css";
+import css from "./mentionsLegales.module.css";
 
 function MentionsLegales_Root() {
     return (
-        <p>test</p>
+        <section className={style.ContainerRootRacine}>
+            <header className={style.ContainerTitle}>
+                <h1 className={style.TitleH1}>
+                    Mentions légales du site PowerWatt34, spécialiste trottinettes électriques
+                </h1>
+            </header>
+
+            <h2 className={style.TitleH2}>
+                1 – Édition du site
+            </h2>
+        </section>
     )
 }
 
