30a3eebe57183075338ec02f9d0ab1b5a085ee9f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-20 11:49:20 +0100

    Push partiel de la page Mentions Légales

--- client/src/components/public/mentions-legales/MentionsLegales.root.tsx
+++ client/src/components/public/mentions-legales/MentionsLegales.root.tsx
@@ -18,14 +18,14 @@ function MentionsLegales_Root() {
             </header>
 
             <h2 className={style.TitleH2_00N0}>
-                1 – Édition du site
+                1 - Édition du site
             </h2>
 
             <p className={style.TextP4}>
                 En vertu de
                 <A_Element option={3} href={routerExt.loi_01} text="l’article 6 de la loi n° 2004-575 du 21 juin 2004" />
                 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site internet
-                <A_Element option={3} href={routerExt.linkPowerWatt34} text={routerExt.linkPowerWatt34} />
+                <A_Element option={3} href={routerExt.linkPowerWatt34} text="www.powerwatt34.fr" />
                 l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi :
             </p>
 
@@ -63,9 +63,47 @@ function MentionsLegales_Root() {
                 <strong>Autre contributeurs :</strong>
             </p>
 
-            <h2 className={style.TitleH2_00N0}>
+            <h2 className={style.TitleH2}>
                 2 - Propriété intellectuelle et contrefaçons
             </h2>
+
+            <p className={style.TextP4}>
+                PowerWatt34 est le propriétaire des droits de propriété intellectuelle et détient les droits
+                d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, 
+                graphismes, logos, vidéos, architecture, icônes et sons.
+            </p>
+
+            <p className={style.TextP4}>
+                Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments 
+                du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable 
+                de PowerWatt34.
+            </p>
+
+            <p className={style.TextP4}>
+                Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée 
+                comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles
+                <A_Element option={1} href={routerExt.loi_02} text="L.335-2 et suivants du Code de Propriété Intellectuelle." />
+            </p>
+
+            <h2 className={style.TitleH2}>
+                3 - Limitation de responsabilité
+            </h2>
+
+            <p className={style.TextP4}>
+                PowerWatt34 ne pourra être tenu pour responsable des dommages directs et indirects 
+                causés au matériel de l’utilisateur, lors de l’accès au site
+                <A_Element option={3} href={routerExt.emailPowerWatt34} text="powerwatt34@gmail.com." />
+                PowerWatt34 décline toute responsabilité quant à l’utilisation qui pourrait 
+                être faite des informations et contenus présents sur
+                <A_Element option={1} href={routerExt.linkPowerWatt34} text="www.powerwatt34.fr" />
+            </p>
+
+            <p className={style.TextP4}>
+                PowerWatt34 s’engage à sécuriser au mieux le site
+                <A_Element option={3} href={routerExt.linkPowerWatt34} text="www.powerwatt34.fr." />
+                Cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont 
+                importées et installées sur son site à son insu.
+            </p>
         </section>
     )
 }
