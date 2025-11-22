1932c6a1e6656d1b96f5e9911b427c2fe3f43a19
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-22 16:43:56 +0100

    Initialisation des composant des vsett

---
+++ client/src/components/public/trottinette/brands/vsett/vsett.data.ts
@@ -0,0 +1,21 @@
+/* Import des Types */
+import type { CardsTrottinetteBrand_data_Type } from "../../../../../types/produits/cardsTrottinetteBrand.data.type";
+
+function kaboo_Data() {
+    const tab: CardsTrottinetteBrand_data_Type = [
+        {
+            reactKey: 1,
+            image: "/images/produits/trottinette/kaboo/kaboo-wolf-king/kaboo-wolf-king-n01-powerwatt34.png",
+            path: "#",
+            model: "Kaboo Wolf King",
+            price: "3699 €",
+            battery: "72V 35Ah", 
+            power: "13440W",
+            range: "130 km",
+        },
+    ]
+
+    return (tab);
+}
+
+export { kaboo_Data };
