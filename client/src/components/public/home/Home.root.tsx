77becfae3e661d831b582160e57013daadecf90f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-26 17:49:36 +0100

    Initialisation de la page Home

--- client/src/components/public/home/Home.root.tsx
+++ client/src/components/public/home/Home.root.tsx
@@ -5,7 +5,14 @@ import style from "../../styleRootComponent.module.css";
 function Home_Root() {
     return (
         <section className={`Home_Root ${style.ContainerRootRacine}`}>
-            <div className={css.ContainerSubAccueil}></div>
+            <div className={css.ContainerSubAccueil}>
+                <header className={css.ContainerTitle}>
+                    <h1 className={`${style.TitleH1} ${css.TitleH1}`}>
+                        Bienvenue chez PowerWatt34 <br />
+                        Spécialiste des 2 roues électriques
+                    </h1>
+                </header>
+            </div>
         </section>
     )
 }
