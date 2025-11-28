1e641103905ba587bcddbc0c11e2e92097ae4d68
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-28 17:57:50 +0100

    Intégration du nouveau composant GeneratorCardAvisClient_Element

---
+++ client/src/components/public/home/dataAvisClient.data.ts
@@ -0,0 +1,22 @@
+/* Import des Types */
+import type { AvisClientSEO_Type } from "../../../types/seo/avisClientSEO.type";
+
+const avisClientHome_Data: AvisClientSEO_Type[] = [
+    /* Commentaire 1 */
+    {
+        id: 1,
+        nom: "A. P.",
+        note: 5,
+        commentaire: "Commerçant sympathique, honnête et très pro. Je recommande à 100%!",
+        date: "28/11/2025",
+    },
+    {
+        id: 2,
+        nom: "jean domenech",
+        note: 5,
+        commentaire: "Commerçant sympathique, honnête et très pro. Je recommande à 100%!",
+        date: "27/11/2025",
+    },
+];
+
+export { avisClientHome_Data };
