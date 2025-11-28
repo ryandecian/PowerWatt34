b161f5547669608a2ef988f3eef456e4921687cd
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-28 15:24:38 +0100

    Ajout des images définitive pour le background image de la page home

--- client/src/components/public/home/Home.root.tsx
+++ client/src/components/public/home/Home.root.tsx
@@ -2,10 +2,20 @@
 import css from "./home.module.css";
 import style from "../../styleRootComponent.module.css";
 
+/* Import des Hooks */
+import useMediaQueriesWidth_rem from "../../../hook/useMediaQueriesWidth_rem.utils.hook";
+
 function Home_Root() {
+    const viewportWidth: number = useMediaQueriesWidth_rem();
+    let backgroundClass: string = "";
+
+    if (viewportWidth > 50) { backgroundClass = css.BackgroundHome_PC }
+    else { backgroundClass = css.BackgroundHome_Mobile }
+
+    
     return (
         <section className={`Home_Root ${style.ContainerRootRacine}`}>
-            <div className={css.ContainerSubAccueil}>
+            <div className={`${css.ContainerSubAccueil} ${backgroundClass}`}>
                 <header className={css.ContainerTitle}>
                     <h1 className={`${style.TitleH1} ${css.TitleH1}`}>
                         Bienvenue chez PowerWatt34 <br />
@@ -13,6 +23,8 @@ function Home_Root() {
                     </h1>
                 </header>
             </div>
+
+            <p>test</p>
         </section>
     )
 }
