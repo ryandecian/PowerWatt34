451842fc59dd1c00442619f81a61bb528a87ba8c
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-28 17:29:26 +0100

    Fin de la création du haut de page de présentation de la page home

--- client/src/components/public/home/Home.root.tsx
+++ client/src/components/public/home/Home.root.tsx
@@ -2,20 +2,11 @@
 import css from "./home.module.css";
 import style from "../../styleRootComponent.module.css";
 
-/* Import des Hooks */
-import useMediaQueriesWidth_rem from "../../../hook/useMediaQueriesWidth_rem.utils.hook";
-
 function Home_Root() {
-    const viewportWidth: number = useMediaQueriesWidth_rem();
-    let backgroundClass: string = "";
-
-    if (viewportWidth > 50) { backgroundClass = css.BackgroundHome_PC }
-    else { backgroundClass = css.BackgroundHome_Mobile }
-
     
     return (
         <section className={`Home_Root ${style.ContainerRootRacine}`}>
-            <div className={`${css.ContainerSubAccueil} ${backgroundClass}`}>
+            <div className={`${css.ContainerSubAccueil} ${css.BackgroundHome}`}>
                 <header className={css.ContainerTitle}>
                     <h1 className={`${style.TitleH1} ${css.TitleH1}`}>
                         Bienvenue chez PowerWatt34 <br />
