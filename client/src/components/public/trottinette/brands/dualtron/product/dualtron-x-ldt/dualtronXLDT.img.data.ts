d1e88391d20362f4c3832545e412433ba7864d3b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-10 17:47:54 +0100

    Mise a jour des types

---
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-x-ldt/dualtronXLDT.img.data.ts
@@ -0,0 +1,35 @@
+
+
+function dualtronXLDT_img_Data() {
+    const tab = [
+        {
+            reactKey: 1,
+            image: "/images/produits/trottinette/dualtron/dualtron-x-ldt/dualtron-x-ltd-n01-powerwatt34.jpg",
+            alt: "Dualtron X LDT - Image 1 - PowerWatt34",
+        },
+        {
+            reactKey: 2,
+            image: "/images/produits/trottinette/dualtron/dualtron-x-ldt/dualtron-x-ltd-n02-powerwatt34.jpg",
+            alt: "Dualtron X LDT - Image 2 - PowerWatt34",
+        },
+        {
+            reactKey: 3,
+            image: "/images/produits/trottinette/dualtron/dualtron-x-ldt/dualtron-x-ltd-n03-powerwatt34.jpg",
+            alt: "Dualtron X LDT - Image 3 - PowerWatt34",
+        },
+        {
+            reactKey: 4,
+            image: "/images/produits/trottinette/dualtron/dualtron-x-ldt/dualtron-x-ltd-n04-powerwatt34.jpg",
+            alt: "Dualtron X LDT - Image 4 - PowerWatt34",
+        },
+        {
+            reactKey: 5,
+            image: "/images/produits/trottinette/dualtron/dualtron-x-ldt/dualtron-x-ltd-n05-powerwatt34.jpg",
+            alt: "Dualtron X LDT - Image 5 - PowerWatt34",
+        },
+    ]
+
+    return tab;
+}
+
+export { dualtronXLDT_img_Data };
