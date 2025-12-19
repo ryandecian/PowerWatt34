7c487a9d8cd3bcd6ac9b10f3ab628629ca454103
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-19 17:47:30 +0100

    Mise a jour de dualtronVictorLTD35Ah_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-ltd-35ah/dualtronVictorLTD35Ah.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-ltd-35ah/dualtronVictorLTD35Ah.data.ts
@@ -5,7 +5,7 @@ function dualtronVictorLTD35Ah_Data(): Trottinette_Data_Type {
     const data: Trottinette_Data_Type = {
         marque: "Dualtron",
         modele: "Victor LTD",
-        batterie: "60V 35Ah (Samsung 21700)",
+        batterie: "60V 35Ah (LG 21700)",
         autonomie: "90 à 120 km",
         moteurs: "Bimoteurs",
         taille_roues: "10 pouces tubeless",
@@ -17,7 +17,7 @@ function dualtronVictorLTD35Ah_Data(): Trottinette_Data_Type {
         clignotants: "Oui",
         poids: "33 kg",
         charge_maximale: "120 kg",
-        prix: "2649 €",
+        prix: "2449 €",
     }
 
     return data;
