99a6c32ab5244aa3d5ec797bc2078bfc8f5d001c
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-19 17:44:06 +0100

    Création des composants du produit dualtron victor ltd 35 Ah

---
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-ltd-35ah/dualtronVictorLTD35Ah.data.ts
@@ -0,0 +1,26 @@
+/* Import des Types */
+import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
+
+function dualtronVictorLTD35Ah_Data(): Trottinette_Data_Type {
+    const data: Trottinette_Data_Type = {
+        marque: "Dualtron",
+        modele: "Victor LTD",
+        batterie: "60V 35Ah (Samsung 21700)",
+        autonomie: "90 à 120 km",
+        moteurs: "Bimoteurs",
+        taille_roues: "10 pouces tubeless",
+        puissance_nominale: "4000W soit 2 x 2000W",
+        puissance_maximale: "8000W",
+        vitesse_bridée: "25 km/h",
+        vitesse_maximale: "80 km/h",
+        freinage: "Hydraulique NUTT à disques 140mm",
+        clignotants: "Oui",
+        poids: "33 kg",
+        charge_maximale: "120 kg",
+        prix: "2649 €",
+    }
+
+    return data;
+}
+
+export { dualtronVictorLTD35Ah_Data };
