8b7b4c462adc9f4b0bb1b2d78dbe536a78109968
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-20 11:00:50 +0100

    Création de DualtronVictorLTD28Ah_Root

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-ltd-28ah/DualtronVictorLTD28Ah.root.tsx
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-ltd-28ah/DualtronVictorLTD28Ah.root.tsx
@@ -6,18 +6,18 @@ import { CarrouselProduct_Element } from "../../../../../../elements/carrousel/C
 import { TableTrottinette_Element } from "../../../../../../elements/table/TableTrottinette.element";
 
 /* Import des Datas */
-import { dualtronVictorLTD31Ah_Data } from "./dualtronVictorLTD28Ah.data";
-import { dualtronVictorLTD31Ah_img_Data } from "./dualtronVictorLTD31Ah.img.data";
+import { dualtronVictorLTD28Ah_Data } from "./dualtronVictorLTD28Ah.data";
+import { dualtronVictorLTD28Ah_img_Data } from "./dualtronVictorLTD28Ah.img.data";
 
 /* Import des Types */
 import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function DualtronVictorLTD31Ah_Root() {
-    const productData: Trottinette_Data_Type = dualtronVictorLTD31Ah_Data();
+function DualtronVictorLTD28Ah_Root() {
+    const productData: Trottinette_Data_Type = dualtronVictorLTD28Ah_Data();
     return (
-        <section className={`DualtronVictorLTD31Ah_Root ${style.ContainerRootRacine}`}>
+        <section className={`DualtronVictorLTD28Ah_Root ${style.ContainerRootRacine}`}>
             <h1 className={style.TitleH1}>{productData.modele}</h1>
-            <CarrouselProduct_Element dataImg={dualtronVictorLTD31Ah_img_Data()} />
+            <CarrouselProduct_Element dataImg={dualtronVictorLTD28Ah_img_Data()} />
             <br />
             <p><strong>Prix : {productData.prix}</strong></p>
             <TableTrottinette_Element data={productData} />
@@ -25,4 +25,4 @@ function DualtronVictorLTD31Ah_Root() {
     );
 }
 
-export default DualtronVictorLTD31Ah_Root;
+export default DualtronVictorLTD28Ah_Root;
