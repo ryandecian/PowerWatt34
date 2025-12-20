eb3f2edeb922fed68e6573f08f02a84cfbcd6446
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-20 11:42:27 +0100

    Création de dualtronVictorLuxuryPlus35Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-luxury-plus-35ah/dualtronVictorLuxuryPlus35Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-luxury-plus-35ah/dualtronVictorLuxuryPlus35Ah.img.data.ts
@@ -1,31 +1,31 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronVictorLuxuryPlus35AhSamsung_img_Data() {
+function dualtronVictorLuxuryPlus35Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
             src: "/images/produits/trottinette/dualtron/dualtron-victor-luxury-plus/dualtron-victor-luxury-plus-n01-powerwatt34.jpg",
-            alt: "Dualtron Victor Luxury Plus 35Ah Samsung - Image 1 - PowerWatt34",
+            alt: "Dualtron Victor Luxury Plus 35Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
             src: "/images/produits/trottinette/dualtron/dualtron-victor-luxury-plus/dualtron-victor-luxury-plus-n02-powerwatt34.jpg",
-            alt: "Dualtron Victor Luxury Plus 35Ah Samsung - Image 2 - PowerWatt34",
+            alt: "Dualtron Victor Luxury Plus 35Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
             src: "/images/produits/trottinette/dualtron/dualtron-victor-luxury-plus/dualtron-victor-luxury-plus-n03-powerwatt34.jpg",
-            alt: "Dualtron Victor Luxury Plus 35Ah Samsung - Image 3 - PowerWatt34",
+            alt: "Dualtron Victor Luxury Plus 35Ah - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
             src: "/images/produits/trottinette/dualtron/dualtron-victor-luxury-plus/dualtron-victor-luxury-plus-n04-powerwatt34.jpg",
-            alt: "Dualtron Victor Luxury Plus 35Ah Samsung - Image 4 - PowerWatt34",
+            alt: "Dualtron Victor Luxury Plus 35Ah - Image 4 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronVictorLuxuryPlus35AhSamsung_img_Data };
+export { dualtronVictorLuxuryPlus35Ah_img_Data };
