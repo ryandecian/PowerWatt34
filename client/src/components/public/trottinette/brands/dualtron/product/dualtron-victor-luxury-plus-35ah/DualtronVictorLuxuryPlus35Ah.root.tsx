a813ab6b57082c6e24f7141ca8a2435d44e85c19
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-20 11:43:00 +0100

    Création de DualtronVictorLuxuryPlus35Ah_Root

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-luxury-plus-35ah/DualtronVictorLuxuryPlus35Ah.root.tsx
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-luxury-plus-35ah/DualtronVictorLuxuryPlus35Ah.root.tsx
@@ -6,18 +6,18 @@ import { CarrouselProduct_Element } from "../../../../../../elements/carrousel/C
 import { TableTrottinette_Element } from "../../../../../../elements/table/TableTrottinette.element";
 
 /* Import des Datas */
-import { dualtronVictorLuxuryPlus35AhSamsung_Data } from "./dualtronVictorLuxuryPlus35AhSamsung.data";
-import { dualtronVictorLuxuryPlus35AhSamsung_img_Data } from "./dualtronVictorLuxuryPlus35AhSamsung.img.data";
+import { dualtronVictorLuxuryPlus35Ah_Data } from "./dualtronVictorLuxuryPlus35Ah.data";
+import { dualtronVictorLuxuryPlus35Ah_img_Data } from "./dualtronVictorLuxuryPlus35Ah.img.data";
 
 /* Import des Types */
 import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function DualtronVictorLuxuryPlus35AhSamsung_Root() {
-    const productData: Trottinette_Data_Type = dualtronVictorLuxuryPlus35AhSamsung_Data();
+function DualtronVictorLuxuryPlus35Ah_Root() {
+    const productData: Trottinette_Data_Type = dualtronVictorLuxuryPlus35Ah_Data();
     return (
-        <section className={`DualtronVictorLuxuryPlus35AhSamsung_Root ${style.ContainerRootRacine}`}>
+        <section className={`DualtronVictorLuxuryPlus35Ah_Root ${style.ContainerRootRacine}`}>
             <h1 className={style.TitleH1}>{productData.modele}</h1>
-            <CarrouselProduct_Element dataImg={dualtronVictorLuxuryPlus35AhSamsung_img_Data()} />
+            <CarrouselProduct_Element dataImg={dualtronVictorLuxuryPlus35Ah_img_Data()} />
             <br />
             <p><strong>Prix : {productData.prix}</strong></p>
             <TableTrottinette_Element data={productData} />
@@ -25,4 +25,4 @@ function DualtronVictorLuxuryPlus35AhSamsung_Root() {
     );
 }
 
-export default DualtronVictorLuxuryPlus35AhSamsung_Root;
+export default DualtronVictorLuxuryPlus35Ah_Root;
