f7a912bec49336a708feef0d485222f22148f2aa
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-20 10:56:29 +0100

    Création de dualtronVictorLTD28Ah_Data

---
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-ltd-28ah/dualtronVictorLTD28Ah.data.ts
@@ -0,0 +1,26 @@
+/* Import des Types */
+import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
+
+function dualtronVictorLTD28Ah_Data(): Trottinette_Data_Type {
+    const data: Trottinette_Data_Type = {
+        marque: "Dualtron",
+        modele: "Victor LTD",
+        batterie: "60V 31Ah (Samsung 21700)",
+        autonomie: "70 à 90 km",
+        moteurs: "Bimoteurs",
+        taille_roues: "10 pouces tubeless",
+        puissance_nominale: "2600W soit 2 x 1300W",
+        puissance_maximale: "4000W soit 2 x 2000W",
+        vitesse_bridée: "25 km/h",
+        vitesse_maximale: "80 km/h",
+        freinage: "Hydraulique NUTT à disques 140mm",
+        clignotants: "Oui",
+        poids: "39,1 kg",
+        charge_maximale: "120 kg",
+        prix: "2249 €",
+    }
+
+    return data;
+}
+
+export { dualtronVictorLTD28Ah_Data };
