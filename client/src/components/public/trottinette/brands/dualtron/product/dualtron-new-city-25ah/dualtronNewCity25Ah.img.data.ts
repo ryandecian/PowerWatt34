200bdffe197f4272e6c18cbaa674b996dbfc6f73
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 16:04:02 +0100

    Création de dualtronNewCity25Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-new-city-25ah/dualtronNewCity25Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-new-city-25ah/dualtronNewCity25Ah.img.data.ts
@@ -1,36 +1,36 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronNewCity22Ah_img_Data() {
+function dualtronNewCity25Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
             src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n01-powerwatt34.jpg",
-            alt: "Dualtron New City 22,5Ah - Image 1 - PowerWatt34",
+            alt: "Dualtron New City 25Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
             src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n02-powerwatt34.jpg",
-            alt: "Dualtron New City 22,5Ah - Image 2 - PowerWatt34",
+            alt: "Dualtron New City 25Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
             src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n03-powerwatt34.jpg",
-            alt: "Dualtron New City 22,5Ah - Image 3 - PowerWatt34",
+            alt: "Dualtron New City 25Ah - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
             src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n04-powerwatt34.jpg",
-            alt: "Dualtron New City 22,5Ah - Image 4 - PowerWatt34",
+            alt: "Dualtron New City 25Ah - Image 4 - PowerWatt34",
         },
         {
             reactKey: 5,
             src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n05-powerwatt34.jpg",
-            alt: "Dualtron New City 22,5Ah - Image 5 - PowerWatt34",
+            alt: "Dualtron New City 25Ah - Image 5 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronNewCity22Ah_img_Data };
+export { dualtronNewCity25Ah_img_Data };
