d4c88c8d514a58ba0cf199637f2f438ea0475825
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 22:57:13 +0100

    Création de DualtronAminia15Ah_Root

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-aminia-15ah/DualtronAminia15Ah.root.tsx
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-aminia-15ah/DualtronAminia15Ah.root.tsx
@@ -6,18 +6,18 @@ import { CarrouselProduct_Element } from "../../../../../../elements/carrousel/C
 import { TableTrottinette_Element } from "../../../../../../elements/table/TableTrottinette.element";
 
 /* Import des Datas */
-import { dualtronAminia21Ah_Data } from "./dualtronAminia21Ah.data";
-import { dualtronAminia21Ah_img_Data } from "./dualtronAminia21Ah.img.data";
+import { dualtronAminia15Ah_Data } from "./dualtronAminia15Ah.data";
+import { dualtronAminia15Ah_img_Data } from "./dualtronAminia15Ah.img.data";
 
 /* Import des Types */
 import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function DualtronAminia21Ah_Root() {
-    const productData: Trottinette_Data_Type = dualtronAminia21Ah_Data();
+function DualtronAminia15Ah_Root() {
+    const productData: Trottinette_Data_Type = dualtronAminia15Ah_Data();
     return (
-        <section className={`DualtronAminia21Ah_Root ${style.ContainerRootRacine}`}>
+        <section className={`DualtronAminia15Ah_Root ${style.ContainerRootRacine}`}>
             <h1 className={style.TitleH1}>{productData.modele}</h1>
-            <CarrouselProduct_Element dataImg={dualtronAminia21Ah_img_Data()} />
+            <CarrouselProduct_Element dataImg={dualtronAminia15Ah_img_Data()} />
             <br />
             <p><strong>Prix : {productData.prix}</strong></p>
             <TableTrottinette_Element data={productData} />
@@ -25,4 +25,4 @@ function DualtronAminia21Ah_Root() {
     );
 }
 
-export default DualtronAminia21Ah_Root;
+export default DualtronAminia15Ah_Root;
