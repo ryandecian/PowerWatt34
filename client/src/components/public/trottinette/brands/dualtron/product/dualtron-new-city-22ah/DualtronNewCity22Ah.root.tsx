e8b5f487558eab0d732e58d014f2e82328931ef7
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 15:59:32 +0100

    Création de DualtronNewCity22Ah_Root

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-new-city-22ah/DualtronNewCity22Ah.root.tsx
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-new-city-22ah/DualtronNewCity22Ah.root.tsx
@@ -6,18 +6,18 @@ import { CarrouselProduct_Element } from "../../../../../../elements/carrousel/C
 import { TableTrottinette_Element } from "../../../../../../elements/table/TableTrottinette.element";
 
 /* Import des Datas */
-import { dualtronNewCity20Ah_Data } from "./dualtronNewCity20Ah.data";
-import { dualtronNewCity20Ah_img_Data } from "./dualtronNewCity20Ah.img.data";
+import { dualtronNewCity22Ah_Data } from "./dualtronNewCity22Ah.data";
+import { dualtronNewCity22Ah_img_Data } from "./dualtronNewCity22Ah.img.data";
 
 /* Import des Types */
 import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function DualtronNewCity20Ah_Root() {
-    const productData: Trottinette_Data_Type = dualtronNewCity20Ah_Data();
+function DualtronNewCity22Ah_Root() {
+    const productData: Trottinette_Data_Type = dualtronNewCity22Ah_Data();
     return (
-        <section className={`DualtronNewCity20Ah_Root ${style.ContainerRootRacine}`}>
+        <section className={`DualtronNewCity22Ah_Root ${style.ContainerRootRacine}`}>
             <h1 className={style.TitleH1}>{productData.modele}</h1>
-            <CarrouselProduct_Element dataImg={dualtronNewCity20Ah_img_Data()} />
+            <CarrouselProduct_Element dataImg={dualtronNewCity22Ah_img_Data()} />
             <br />
             <p><strong>Prix : {productData.prix}</strong></p>
             <TableTrottinette_Element data={productData} />
@@ -25,4 +25,4 @@ function DualtronNewCity20Ah_Root() {
     );
 }
 
-export default DualtronNewCity20Ah_Root;
+export default DualtronNewCity22Ah_Root;
