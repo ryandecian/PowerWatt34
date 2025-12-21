4eef2c920abaaf19c387df250ce3f1b403ff6e3a
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 16:07:28 +0100

    Création de DualtronNewCity20Ah_Page

---
+++ client/src/pages/public/trottinette_Page/brands/dualtron_Page/product/DualtronNewCity20Ah.page.tsx
@@ -0,0 +1,30 @@
+/* Import des modules CSS */
+import style from "../../../../../stylePage.module.css";
+
+/* Import des Components */
+import Nav_root_Layout from "../../../../../../components/layout/nav_Layout/Nav.root.layout";
+import DualtronNewCity20Ah_Root from "../../../../../../components/public/trottinette/brands/dualtron/product/dualtron-new-city-20ah/DualtronNewCity20Ah.root";
+import Footer_root_layout from "../../../../../../components/layout/footer_Layout/Footer.root.layout";
+
+/* Import des Hooks */
+import { useScrollToHash_utils_Hook } from "../../../../../../hook/useScrollToHash.utils.hook";
+
+function DualtronNewCity20Ah_Page() {
+    useScrollToHash_utils_Hook(); /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
+
+    return (
+        <section className={`DualtronNewCity20Ah_Page ${style.Page}`}>
+            <header id="top" className={style.Header}>
+                <Nav_root_Layout />
+            </header>
+            <main className={style.Main}>
+                <DualtronNewCity20Ah_Root />
+            </main>
+            <footer className={style.Footer}>
+                <Footer_root_layout />
+            </footer>
+        </section>
+    );
+}
+
+export default DualtronNewCity20Ah_Page;
