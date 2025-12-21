1b69acd58ddcb528d989e9f7b55cb333417ba3e7
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 23:17:25 +0100

    Création de dualtronAminia13Ah_img_Data[3~

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-aminia-13ah/dualtronAminia13Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-aminia-13ah/dualtronAminia13Ah.img.data.ts
@@ -1,61 +1,61 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronAminia17Ah_img_Data() {
+function dualtronAminia13Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n01-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 1 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n02-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 2 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n03-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 3 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n04-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 4 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 4 - PowerWatt34",
         },
         {
             reactKey: 5,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n05-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 5 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 5 - PowerWatt34",
         },
         {
             reactKey: 6,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n06-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 6 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 6 - PowerWatt34",
         },
         {
             reactKey: 7,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n07-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 7 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 7 - PowerWatt34",
         },
         {
             reactKey: 8,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n08-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 8 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 8 - PowerWatt34",
         },
         {
             reactKey: 9,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n09-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 9 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 9 - PowerWatt34",
         },
         {
             reactKey: 10,
             src: "/images/produits/trottinette/dualtron/dualtron-aminia/dualtron-aminia-n10-powerwatt34.jpg",
-            alt: "Dualtron Aminia - 17,5Ah - Image 10 - PowerWatt34",
+            alt: "Dualtron Aminia - 13Ah - Image 10 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronAminia17Ah_img_Data };
+export { dualtronAminia13Ah_img_Data };
