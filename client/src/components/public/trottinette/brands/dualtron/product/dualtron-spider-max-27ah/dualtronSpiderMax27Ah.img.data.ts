254bb5ab6dc293f4bc53594de9423d40423d881d
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 19:22:37 +0100

    Mise a jour de dualtronSpiderMax27Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-spider-max-27ah/dualtronSpiderMax27Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-spider-max-27ah/dualtronSpiderMax27Ah.img.data.ts
@@ -6,42 +6,42 @@ function dualtronSpiderMax27Ah_img_Data() {
         {
             reactKey: 1,
             src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n01-powerwatt34.jpg",
-            alt: "Dualtron Spider Max - Image 1 - PowerWatt34",
+            alt: "Dualtron Spider Max - 27Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
             src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n02-powerwatt34.jpg",
-            alt: "Dualtron Spider Max - Image 2 - PowerWatt34",
+            alt: "Dualtron Spider Max - 27Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
             src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n03-powerwatt34.jpg",
-            alt: "Dualtron Spider Max - Image 3 - PowerWatt34",
+            alt: "Dualtron Spider Max - 27Ah - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
             src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n04-powerwatt34.jpg",
-            alt: "Dualtron Spider Max - Image 4 - PowerWatt34",
+            alt: "Dualtron Spider Max - 27Ah - Image 4 - PowerWatt34",
         },
         {
             reactKey: 5,
             src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n05-powerwatt34.jpg",
-            alt: "Dualtron Spider Max - Image 5 - PowerWatt34",
+            alt: "Dualtron Spider Max - 27Ah - Image 5 - PowerWatt34",
         },
         {
             reactKey: 6,
             src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n06-powerwatt34.jpg",
-            alt: "Dualtron Spider Max - Image 6 - PowerWatt34",
+            alt: "Dualtron Spider Max - 27Ah - Image 6 - PowerWatt34",
         },
         {
             reactKey: 7,
             src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n07-powerwatt34.jpg",
-            alt: "Dualtron Spider Max - Image 7 - PowerWatt34",
+            alt: "Dualtron Spider Max - 27Ah - Image 7 - PowerWatt34",
         },
         {
             reactKey: 8,
             src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n08-powerwatt34.jpg",
-            alt: "Dualtron Spider Max - Image 8 - PowerWatt34",
+            alt: "Dualtron Spider Max - 27Ah - Image 8 - PowerWatt34",
         },
     ]
 
