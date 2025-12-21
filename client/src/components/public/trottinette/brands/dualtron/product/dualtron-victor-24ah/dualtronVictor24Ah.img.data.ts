de89549dc5308149cb2c2950eba791ea9b88eba0
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 19:56:22 +0100

    Création de dualtronVictor24Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-24ah/dualtronVictor24Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-victor-24ah/dualtronVictor24Ah.img.data.ts
@@ -1,36 +1,51 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronNewCity20Ah_img_Data() {
+function dualtronVictor24Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
-            src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n01-powerwatt34.jpg",
-            alt: "Dualtron New City 20Ah - Image 1 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-victor/dualtron-victor-n01-powerwatt34.jpg",
+            alt: "Dualtron Victor 24Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
-            src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n02-powerwatt34.jpg",
-            alt: "Dualtron New City 20Ah - Image 2 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-victor/dualtron-victor-n02-powerwatt34.jpg",
+            alt: "Dualtron Victor 24Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
-            src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n03-powerwatt34.jpg",
-            alt: "Dualtron New City 20Ah - Image 3 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-victor/dualtron-victor-n03-powerwatt34.jpg",
+            alt: "Dualtron Victor 24Ah - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
-            src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n04-powerwatt34.jpg",
-            alt: "Dualtron New City 20Ah - Image 4 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-victor/dualtron-victor-n04-powerwatt34.jpg",
+            alt: "Dualtron Victor 24Ah - Image 4 - PowerWatt34",
         },
         {
             reactKey: 5,
-            src: "/images/produits/trottinette/dualtron/dualtron-new-city/dualtron-new-city-n05-powerwatt34.jpg",
-            alt: "Dualtron New City 20Ah - Image 5 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-victor/dualtron-victor-n05-powerwatt34.jpg",
+            alt: "Dualtron Victor 24Ah - Image 5 - PowerWatt34",
+        },
+        {
+            reactKey: 6,
+            src: "/images/produits/trottinette/dualtron/dualtron-victor/dualtron-victor-n06-powerwatt34.jpg",
+            alt: "Dualtron Victor 24Ah - Image 6 - PowerWatt34",
+        },
+        {
+            reactKey: 7,
+            src: "/images/produits/trottinette/dualtron/dualtron-victor/dualtron-victor-n07-powerwatt34.jpg",
+            alt: "Dualtron Victor 24Ah - Image 7 - PowerWatt34",
+        },
+        {
+            reactKey: 8,
+            src: "/images/produits/trottinette/dualtron/dualtron-victor/dualtron-victor-n08-powerwatt34.jpg",
+            alt: "Dualtron Victor 24Ah - Image 8 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronNewCity20Ah_img_Data };
+export { dualtronVictor24Ah_img_Data };
