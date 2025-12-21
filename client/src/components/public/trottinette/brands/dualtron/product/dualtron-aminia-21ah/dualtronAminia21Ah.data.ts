6aff167390fb3d2d1bf4c52de2ed70d241cf37dd
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 23:02:47 +0100

    Mise a jour de dualtronAminia21Ah_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-aminia-21ah/dualtronAminia21Ah.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-aminia-21ah/dualtronAminia21Ah.data.ts
@@ -17,7 +17,7 @@ function dualtronAminia21Ah_Data(): Trottinette_Data_Type {
         clignotants: "Oui",
         poids: "30 kg",
         charge_maximale: "120 kg",
-        prix: "1899 €",
+        prix: "1349 €",
     }
 
     return data;
