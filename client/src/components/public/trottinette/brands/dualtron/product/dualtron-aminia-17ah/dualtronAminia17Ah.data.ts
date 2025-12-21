a4b5803ca89bf2afca282f4aca8bc4dcfba9d89a
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 23:11:32 +0100

    Mise a jour de dualtronAminia17Ah_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-aminia-17ah/dualtronAminia17Ah.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-aminia-17ah/dualtronAminia17Ah.data.ts
@@ -12,7 +12,7 @@ function dualtronAminia17Ah_Data(): Trottinette_Data_Type {
         puissance_nominale: "1000W",
         puissance_maximale: "1450W",
         vitesse_bridée: "25 km/h",
-        vitesse_maximale: "65 km/h",
+        vitesse_maximale: "45 km/h",
         freinage: "Hydraulique à disques 140mm + frein électronique",
         clignotants: "Oui",
         poids: "30 kg",
