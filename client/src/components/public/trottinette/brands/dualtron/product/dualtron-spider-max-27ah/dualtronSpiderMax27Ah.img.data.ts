e0943c00e5ed4fbe58817c8d7803d551d68d8011
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 19:16:12 +0100

    Création de dualtronSpiderMax27Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-spider-max-27ah/dualtronSpiderMax27Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-spider-max-27ah/dualtronSpiderMax27Ah.img.data.ts
@@ -1,46 +1,46 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronThunder3_img_Data() {
+function dualtronSpiderMax27Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
-            src: "/images/produits/trottinette/dualtron/dualtron-thunder-3/dualtron-thunder-3-n01-powerwatt34.webp",
-            alt: "Dualtron Thunder 3 - Image 1 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n01-powerwatt34.jpg",
+            alt: "Dualtron Spider Max - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
-            src: "/images/produits/trottinette/dualtron/dualtron-thunder-3/dualtron-thunder-3-n02-powerwatt34.jpg",
-            alt: "Dualtron Thunder 3 - Image 2 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n02-powerwatt34.jpg",
+            alt: "Dualtron Spider Max - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
-            src: "/images/produits/trottinette/dualtron/dualtron-thunder-3/dualtron-thunder-3-n03-powerwatt34.jpg",
-            alt: "Dualtron Thunder 3 - Image 3 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n03-powerwatt34.jpg",
+            alt: "Dualtron Spider Max - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
-            src: "/images/produits/trottinette/dualtron/dualtron-thunder-3/dualtron-thunder-3-n04-powerwatt34.jpg",
-            alt: "Dualtron Thunder 3 - Image 4 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n04-powerwatt34.jpg",
+            alt: "Dualtron Spider Max - Image 4 - PowerWatt34",
         },
         {
             reactKey: 5,
-            src: "/images/produits/trottinette/dualtron/dualtron-thunder-3/dualtron-thunder-3-n05-powerwatt34.jpg",
-            alt: "Dualtron Thunder 3 - Image 5 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n05-powerwatt34.jpg",
+            alt: "Dualtron Spider Max - Image 5 - PowerWatt34",
         },
         {
             reactKey: 6,
-            src: "/images/produits/trottinette/dualtron/dualtron-thunder-3/dualtron-thunder-3-n06-powerwatt34.jpg",
-            alt: "Dualtron Thunder 3 - Image 6 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n06-powerwatt34.jpg",
+            alt: "Dualtron Spider Max - Image 6 - PowerWatt34",
         },
         {
             reactKey: 7,
-            src: "/images/produits/trottinette/dualtron/dualtron-thunder-3/dualtron-thunder-3-n07-powerwatt34.jpg",
-            alt: "Dualtron Thunder 3 - Image 7 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-spider-max/dualtron-spider-max-n07-powerwatt34.jpg",
+            alt: "Dualtron Spider Max - Image 7 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronThunder3_img_Data };
+export { dualtronSpiderMax27Ah_img_Data };
