9e599311dce47f1a0bedd6a47add97645ef61651
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-21 15:41:10 +0100

    Création de dualtronNewCity20Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-new-city-20ah/dualtronNewCity20Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-new-city-20ah/dualtronNewCity20Ah.img.data.ts
@@ -1,51 +1,36 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronCity_img_Data() {
+function dualtronNewCity20Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
-            src: "/images/produits/trottinette/dualtron/dualtron-city/dualtron-city-n01-powerwatt34.jpg",
-            alt: "Dualtron City - Image 1 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-new-city-20ah/dualtron-new-city-20ah-n01-powerwatt34.jpg",
+            alt: "Dualtron New City 20Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
-            src: "/images/produits/trottinette/dualtron/dualtron-city/dualtron-city-n02-powerwatt34.jpg",
-            alt: "Dualtron City - Image 2 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-new-city-20ah/dualtron-new-city-20ah-n02-powerwatt34.jpg",
+            alt: "Dualtron New City 20Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
-            src: "/images/produits/trottinette/dualtron/dualtron-city/dualtron-city-n03-powerwatt34.jpg",
-            alt: "Dualtron City - Image 3 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-new-city-20ah/dualtron-new-city-20ah-n03-powerwatt34.jpg",
+            alt: "Dualtron New City 20Ah - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
-            src: "/images/produits/trottinette/dualtron/dualtron-city/dualtron-city-n04-powerwatt34.jpg",
-            alt: "Dualtron City - Image 4 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-new-city-20ah/dualtron-new-city-20ah-n04-powerwatt34.jpg",
+            alt: "Dualtron New City 20Ah - Image 4 - PowerWatt34",
         },
         {
             reactKey: 5,
-            src: "/images/produits/trottinette/dualtron/dualtron-city/dualtron-city-n05-powerwatt34.jpg",
-            alt: "Dualtron City - Image 5 - PowerWatt34",
-        },
-        {
-            reactKey: 6,
-            src: "/images/produits/trottinette/dualtron/dualtron-city/dualtron-city-n06-powerwatt34.jpg",
-            alt: "Dualtron City - Image 6 - PowerWatt34",
-        },
-        {
-            reactKey: 7,
-            src: "/images/produits/trottinette/dualtron/dualtron-city/dualtron-city-n07-powerwatt34.jpg",
-            alt: "Dualtron City - Image 7 - PowerWatt34",
-        },
-        {
-            reactKey: 8,
-            src: "/images/produits/trottinette/dualtron/dualtron-city/dualtron-city-n08-powerwatt34.jpg",
-            alt: "Dualtron City - Image 8 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-new-city-20ah/dualtron-new-city-20ah-n05-powerwatt34.jpg",
+            alt: "Dualtron New City 20Ah - Image 5 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronCity_img_Data };
+export { dualtronNewCity20Ah_img_Data };
