3952fdac8d5d89eb253fc3ea4ef8332fda12bed4
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-23 15:27:14 +0100

    Mise a jour de DualtronForeverLimited18Ah_Page

--- client/src/pages/public/trottinette_Page/brands/dualtron_Page/product/DualtronForeverLimited18Ah.page.tsx
+++ client/src/pages/public/trottinette_Page/brands/dualtron_Page/product/DualtronForeverLimited18Ah.page.tsx
@@ -3,22 +3,22 @@ import style from "../../../../../stylePage.module.css";
 
 /* Import des Components */
 import Nav_root_Layout from "../../../../../../components/layout/nav_Layout/Nav.root.layout";
-import DualtronForeverLimited24Ah_Root from "../../../../../../components/public/trottinette/brands/dualtron/product/dualtron-forever-limited-24ah/DualtronForeverLimited24Ah.root";
+import DualtronForeverLimited18Ah_Root from "../../../../../../components/public/trottinette/brands/dualtron/product/dualtron-forever-limited-18ah/DualtronForeverLimited18Ah.root";
 import Footer_root_layout from "../../../../../../components/layout/footer_Layout/Footer.root.layout";
 
 /* Import des Hooks */
 import { useScrollToHash_utils_Hook } from "../../../../../../hook/useScrollToHash.utils.hook";
 
-function DualtronForeverLimited24Ah_Page() {
+function DualtronForeverLimited18Ah_Page() {
     useScrollToHash_utils_Hook(); /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
 
     return (
-        <section className={`DualtronForeverLimited24Ah_Page ${style.Page}`}>
+        <section className={`DualtronForeverLimited18Ah_Page ${style.Page}`}>
             <header id="top" className={style.Header}>
                 <Nav_root_Layout />
             </header>
             <main className={style.Main}>
-                <DualtronForeverLimited24Ah_Root />
+                <DualtronForeverLimited18Ah_Root />
             </main>
             <footer className={style.Footer}>
                 <Footer_root_layout />
@@ -27,4 +27,4 @@ function DualtronForeverLimited24Ah_Page() {
     );
 }
 
-export default DualtronForeverLimited24Ah_Page;
+export default DualtronForeverLimited18Ah_Page;
