c8d3d69023d63f235d62a2c474a01a6832254962
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-23 14:44:38 +0100

    Création de DualtronForeverLimited18Ah_Root

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-forever-limited-18ah/DualtronForeverLimited18Ah.root.tsx
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-forever-limited-18ah/DualtronForeverLimited18Ah.root.tsx
@@ -2,22 +2,22 @@
 import style from "../../../../../../styleRootComponent.module.css";
 
 /* Import des composants d'Elements */
-import { CarrouselProduct_Element } from "../../../../../../../elements/carrousel/CarrouselProduct.element";
-import { TableTrottinette_Element } from "../../../../../../../elements/table/TableTrottinette.element";
+import { CarrouselProduct_Element } from "../../../../../../elements/carrousel/CarrouselProduct.element";
+import { TableTrottinette_Element } from "../../../../../../elements/table/TableTrottinette.element";
 
 /* Import des Datas */
-import { dualtronForeverLimited24Ah_Data } from "./dualtronForeverLimited24Ah.data";
-import { dualtronForeverLimited24Ah_img_Data } from "./dualtronForeverLimited24Ah.img.data";
+import { dualtronForeverLimited18Ah_Data } from "./dualtronForeverLimited18Ah.data";
+import { dualtronForeverLimited18Ah_img_Data } from "./dualtronForeverLimited18Ah.img.data";
 
 /* Import des Types */
-import type { Trottinette_Data_Type } from "../../../../../../../../types/produits/trottinette/trottinette.data.type";
+import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function DualtronForeverLimited24Ah_Root() {
-    const productData: Trottinette_Data_Type = dualtronForeverLimited24Ah_Data();
+function DualtronForeverLimited18Ah_Root() {
+    const productData: Trottinette_Data_Type = dualtronForeverLimited18Ah_Data();
     return (
-        <section className={`DualtronForeverLimited24Ah_Root ${style.ContainerRootRacine}`}>
+        <section className={`DualtronForeverLimited18Ah_Root ${style.ContainerRootRacine}`}>
             <h1 className={style.TitleH1}>{productData.modele}</h1>
-            <CarrouselProduct_Element dataImg={dualtronForeverLimited24Ah_img_Data()} />
+            <CarrouselProduct_Element dataImg={dualtronForeverLimited18Ah_img_Data()} />
             <br />
             <p><strong>Prix : {productData.prix}</strong></p>
             <TableTrottinette_Element data={productData} />
@@ -25,4 +25,4 @@ function DualtronForeverLimited24Ah_Root() {
     );
 }
 
-export default DualtronForeverLimited24Ah_Root;
+export default DualtronForeverLimited18Ah_Root;
