f00e578d2fb245854a773be29fe3b42615e6ad21
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-23 14:32:43 +0100

    Création de dualtronForeverLimited24Ah_Data

---
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-forever-limited-24ah/dualtronForeverLimited27Ah.data.ts
@@ -0,0 +1,26 @@
+/* Import des Types */
+import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
+
+function dualtronForeverLimited24Ah_Data(): Trottinette_Data_Type {
+    const data: Trottinette_Data_Type = {
+        marque: "Dualtron",
+        modele: "Dualtron Forever Limited",
+        batterie: "60V 24Ah (LG 21700)",
+        autonomie: "45 à 60 km",
+        moteurs: "Bimoteurs",
+        taille_roues: "10 pouces tubeless",
+        puissance_nominale: "1800W soit 2 x 900W",
+        puissance_maximale: "2900W soit 2 x 1450W",
+        vitesse_bridée: "25 km/h",
+        vitesse_maximale: "65 km/h",
+        freinage: "Hydraulique à disques",
+        clignotants: "Oui",
+        poids: "27 kg",
+        charge_maximale: "100 kg",
+        prix: "1349 €",
+    }
+
+    return data;
+}
+
+export { dualtronForeverLimited24Ah_Data };
