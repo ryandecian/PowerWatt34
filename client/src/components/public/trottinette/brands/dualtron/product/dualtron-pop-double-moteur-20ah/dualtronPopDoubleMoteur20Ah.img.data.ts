bd1239d2c9257e30ae67b4cfb1ca9296e607aa2b
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-23 22:26:52 +0100

    Création de dualtronPopDoubleMoteur20Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-pop-double-moteur-20ah/dualtronPopDoubleMoteur20Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-pop-double-moteur-20ah/dualtronPopDoubleMoteur20Ah.img.data.ts
@@ -1,26 +1,36 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronPop25Ah_img_Data() {
+function dualtronPopDoubleMoteur20Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
-            src: "/images/produits/trottinette/dualtron/dualtron-pop/dualtron-pop-n01-powerwatt34.jpg",
-            alt: "Dualtron Pop 25Ah - Image 1 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-pop-double-moteur/dualtron-pop-double-moteur-n01-powerwatt34.jpg",
+            alt: "Dualtron Pop Double Moteur 20Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
-            src: "/images/produits/trottinette/dualtron/dualtron-pop/dualtron-pop-n02-powerwatt34.jpg",
-            alt: "Dualtron Pop 25Ah - Image 2 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-pop-double-moteur/dualtron-pop-double-moteur-n02-powerwatt34.jpg",
+            alt: "Dualtron Pop Double Moteur 20Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
-            src: "/images/produits/trottinette/dualtron/dualtron-pop/dualtron-pop-n03-powerwatt34.jpg",
-            alt: "Dualtron Pop 25Ah - Image 3 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-pop-double-moteur/dualtron-pop-double-moteur-n03-powerwatt34.jpg",
+            alt: "Dualtron Pop Double Moteur 20Ah - Image 3 - PowerWatt34",
+        },
+        {
+            reactKey: 4,
+            src: "/images/produits/trottinette/dualtron/dualtron-pop-double-moteur/dualtron-pop-double-moteur-n03-powerwatt34.jpg",
+            alt: "Dualtron Pop Double Moteur 20Ah - Image 3 - PowerWatt34",
+        },
+        {
+            reactKey: 5,
+            src: "/images/produits/trottinette/dualtron/dualtron-pop-double-moteur/dualtron-pop-double-moteur-n03-powerwatt34.jpg",
+            alt: "Dualtron Pop Double Moteur 20Ah - Image 3 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronPop25Ah_img_Data };
+export { dualtronPopDoubleMoteur20Ah_img_Data };
