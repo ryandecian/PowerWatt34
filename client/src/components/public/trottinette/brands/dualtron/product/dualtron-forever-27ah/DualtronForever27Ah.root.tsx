57825d244c5803a50acc1d461fd795ed1d9f42e4
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-23 15:58:56 +0100

    Création de DualtronForever27Ah_Root

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-forever-27ah/DualtronForever27Ah.root.tsx
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-forever-27ah/DualtronForever27Ah.root.tsx
@@ -6,18 +6,18 @@ import { CarrouselProduct_Element } from "../../../../../../elements/carrousel/C
 import { TableTrottinette_Element } from "../../../../../../elements/table/TableTrottinette.element";
 
 /* Import des Datas */
-import { dualtronForeverLimited18Ah_Data } from "./dualtronForeverLimited18Ah.data";
-import { dualtronForeverLimited18Ah_img_Data } from "./dualtronForeverLimited18Ah.img.data";
+import { dualtronForever27Ah_Data } from "./dualtronForever27Ah.data";
+import { dualtronForever27Ah_img_Data } from "./dualtronForever27Ah.img.data";
 
 /* Import des Types */
 import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function DualtronForeverLimited18Ah_Root() {
-    const productData: Trottinette_Data_Type = dualtronForeverLimited18Ah_Data();
+function DualtronForever27Ah_Root() {
+    const productData: Trottinette_Data_Type = dualtronForever27Ah_Data();
     return (
-        <section className={`DualtronForeverLimited18Ah_Root ${style.ContainerRootRacine}`}>
+        <section className={`DualtronForever27Ah_Root ${style.ContainerRootRacine}`}>
             <h1 className={style.TitleH1}>{productData.modele}</h1>
-            <CarrouselProduct_Element dataImg={dualtronForeverLimited18Ah_img_Data()} />
+            <CarrouselProduct_Element dataImg={dualtronForever27Ah_img_Data()} />
             <br />
             <p><strong>Prix : {productData.prix}</strong></p>
             <TableTrottinette_Element data={productData} />
@@ -25,4 +25,4 @@ function DualtronForeverLimited18Ah_Root() {
     );
 }
 
-export default DualtronForeverLimited18Ah_Root;
+export default DualtronForever27Ah_Root;
