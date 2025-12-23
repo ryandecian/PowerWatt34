6dbe44e32bd47645d8549b3ad163679062c102af
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-23 22:25:43 +0100

    Création de dualtronPopDoubleMoteur20Ah_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-pop-double-moteur-20ah/dualtronPopDoubleMoteur20Ah.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-pop-double-moteur-20ah/dualtronPopDoubleMoteur20Ah.data.ts
@@ -1,26 +1,26 @@
 /* Import des Types */
 import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function dualtronPop25Ah_Data(): Trottinette_Data_Type {
+function dualtronPopDoubleMoteur20Ah_Data(): Trottinette_Data_Type {
     const data: Trottinette_Data_Type = {
         marque: "Dualtron",
         modele: "Dualtron Pop",
-        batterie: "52V 25Ah (LG)",
+        batterie: "52V 20Ah (LG)",
         autonomie: "35 à 50 km",
-        moteurs: "Monomoteur",
+        moteurs: "Bimoteur",
         taille_roues: "9 pouces tubeless",
-        puissance_nominale: "900W",
-        puissance_maximale: "1450W",
+        puissance_nominale: "1800W soit 2 x 900W",
+        puissance_maximale: "2900W soit 2 x 1450W",
         vitesse_bridée: "25 km/h",
-        vitesse_maximale: "45 km/h",
+        vitesse_maximale: "60 km/h",
         freinage: "Tambour AV + AR + frein électronique",
         clignotants: "Oui",
         poids: "30 kg",
         charge_maximale: "100 kg",
-        prix: "1049 €",
+        prix: "1249 €",
     }
 
     return data;
 }
 
-export { dualtronPop25Ah_Data };
+export { dualtronPopDoubleMoteur20Ah_Data };
