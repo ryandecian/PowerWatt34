547821a3ba4aaec6fdd297256d6d8b473ed7e74d
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-23 23:12:35 +0100

    Création de dualtronTogoLTD15AhSamsung_Data

---
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-togo-ltd-15ah-samsung/dualtronTogoLTD15AhSamsung.data.ts
@@ -0,0 +1,26 @@
+/* Import des Types */
+import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
+
+function dualtronTogoLTD15AhSamsung_Data(): Trottinette_Data_Type {
+    const data: Trottinette_Data_Type = {
+        marque: "Dualtron",
+        modele: "Dualtron Togo LTD 15Ah Samsung",
+        batterie: "60V 15Ah (Samsung)",
+        autonomie: "35 à 50 km",
+        moteurs: "Monomoteur",
+        taille_roues: "9 pouces tubeless",
+        puissance_nominale: "900W",
+        puissance_maximale: "1000W",
+        vitesse_bridée: "25 km/h",
+        vitesse_maximale: "52 km/h",
+        freinage: "Disque AV + AR + frein électronique",
+        clignotants: "Non",
+        poids: "25 kg",
+        charge_maximale: "100 kg",
+        prix: "879 €",
+    }
+
+    return data;
+}
+
+export { dualtronTogoLTD15AhSamsung_Data };
