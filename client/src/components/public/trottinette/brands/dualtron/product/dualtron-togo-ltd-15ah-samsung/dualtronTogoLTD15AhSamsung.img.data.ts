2aa93997cd9bfd55fa692356793e4a765bde6693
Ryan DECIAN <ryan.decian.propgmail.com>
2025-12-23 23:14:54 +0100

    Création de dualtronTogoLTD15AhSamsung_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-togo-ltd-15ah-samsung/dualtronTogoLTD15AhSamsung.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-togo-ltd-15ah-samsung/dualtronTogoLTD15AhSamsung.img.data.ts
@@ -1,61 +1,46 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronTogoMax16Ah_img_Data() {
+function dualtronTogoLTD15AhSamsung_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n01-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 1 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n01-powerwatt34.jpg",
+            alt: "Dualtron Togo LTD 15Ah Samsung - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n02-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 2 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n02-powerwatt34.jpg",
+            alt: "Dualtron Togo LTD 15Ah Samsung - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n03-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 3 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n03-powerwatt34.jpg",
+            alt: "Dualtron Togo LTD 15Ah Samsung - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n04-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 4 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n04-powerwatt34.jpg",
+            alt: "Dualtron Togo LTD 15Ah Samsung - Image 4 - PowerWatt34",
         },
         {
             reactKey: 5,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n05-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 5 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n05-powerwatt34.jpg",
+            alt: "Dualtron Togo LTD 15Ah Samsung - Image 5 - PowerWatt34",
         },
         {
             reactKey: 6,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n06-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 6 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n06-powerwatt34.jpg",
+            alt: "Dualtron Togo LTD 15Ah Samsung - Image 6 - PowerWatt34",
         },
         {
             reactKey: 7,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n07-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 7 - PowerWatt34",
-        },
-        {
-            reactKey: 8,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n08-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 8 - PowerWatt34",
-        },
-        {
-            reactKey: 9,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n09-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 9 - PowerWatt34",
-        },
-        {
-            reactKey: 10,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-max/dualtron-togo-max-n10-powerwatt34.jpg",
-            alt: "Dualtron Togo Max 15,9Ah - Image 10 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n07-powerwatt34.jpg",
+            alt: "Dualtron Togo LTD 15Ah Samsung - Image 7 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronTogoMax16Ah_img_Data };
+export { dualtronTogoLTD15AhSamsung_img_Data };
