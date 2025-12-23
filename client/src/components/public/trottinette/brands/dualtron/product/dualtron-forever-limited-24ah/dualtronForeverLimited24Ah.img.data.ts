1d793a2628d8e703a7226adcd97434ceee170b34
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-23 14:33:29 +0100

    Création de dualtronForeverLimited24Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-forever-limited-24ah/dualtronForeverLimited24Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-forever-limited-24ah/dualtronForeverLimited24Ah.img.data.ts
@@ -1,41 +1,41 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronForeverLimited27Ah_img_Data() {
+function dualtronForeverLimited24Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
             src: "/images/produits/trottinette/dualtron/dualtron-forever-limited/dualtron-forever-limited-n01-powerwatt34.jpg",
-            alt: "Dualtron Forever Limited 27Ah - Image 1 - PowerWatt34",
+            alt: "Dualtron Forever Limited 24Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
             src: "/images/produits/trottinette/dualtron/dualtron-forever-limited/dualtron-forever-limited-n02-powerwatt34.jpg",
-            alt: "Dualtron Forever Limited 27Ah - Image 2 - PowerWatt34",
+            alt: "Dualtron Forever Limited 24Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
             src: "/images/produits/trottinette/dualtron/dualtron-forever-limited/dualtron-forever-limited-n03-powerwatt34.jpg",
-            alt: "Dualtron Forever Limited 27Ah - Image 3 - PowerWatt34",
+            alt: "Dualtron Forever Limited 24Ah - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
             src: "/images/produits/trottinette/dualtron/dualtron-forever-limited/dualtron-forever-limited-n04-powerwatt34.jpg",
-            alt: "Dualtron Forever Limited 27Ah - Image 4 - PowerWatt34",
+            alt: "Dualtron Forever Limited 24Ah - Image 4 - PowerWatt34",
         },
         {
             reactKey: 5,
             src: "/images/produits/trottinette/dualtron/dualtron-forever-limited/dualtron-forever-limited-n05-powerwatt34.jpg",
-            alt: "Dualtron Forever Limited 27Ah - Image 5 - PowerWatt34",
+            alt: "Dualtron Forever Limited 24Ah - Image 5 - PowerWatt34",
         },
         {
             reactKey: 6,
             src: "/images/produits/trottinette/dualtron/dualtron-forever-limited/dualtron-forever-limited-n06-powerwatt34.jpg",
-            alt: "Dualtron Forever Limited 27Ah - Image 6 - PowerWatt34",
+            alt: "Dualtron Forever Limited 24Ah - Image 6 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronForeverLimited27Ah_img_Data };
+export { dualtronForeverLimited24Ah_img_Data };
