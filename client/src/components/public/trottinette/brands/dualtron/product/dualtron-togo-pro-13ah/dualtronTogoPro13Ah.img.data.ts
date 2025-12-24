c8b674b90bcf5533a5e3708833663b15c63579ba
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-24 11:45:29 +0100

    Création de dualtronTogoPro13Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-togo-pro-13ah/dualtronTogoPro13Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-togo-pro-13ah/dualtronTogoPro13Ah.img.data.ts
@@ -1,46 +1,56 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronTogoLTD12Ah_img_Data() {
+function dualtronTogoPro13Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n01-powerwatt34.jpg",
-            alt: "Dualtron Togo LTD 12Ah - Image 1 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n01-powerwatt34.jpg",
+            alt: "Dualtron Togo Pro 13Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n02-powerwatt34.jpg",
-            alt: "Dualtron Togo LTD 12Ah - Image 2 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n02-powerwatt34.jpg",
+            alt: "Dualtron Togo Pro 13Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n03-powerwatt34.jpg",
-            alt: "Dualtron Togo LTD 12Ah - Image 3 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n03-powerwatt34.jpg",
+            alt: "Dualtron Togo Pro 13Ah - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n04-powerwatt34.jpg",
-            alt: "Dualtron Togo LTD 12Ah - Image 4 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n04-powerwatt34.jpg",
+            alt: "Dualtron Togo Pro 13Ah - Image 4 - PowerWatt34",
         },
         {
             reactKey: 5,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n05-powerwatt34.jpg",
-            alt: "Dualtron Togo LTD 12Ah - Image 5 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n05-powerwatt34.jpg",
+            alt: "Dualtron Togo Pro 13Ah - Image 5 - PowerWatt34",
         },
         {
             reactKey: 6,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n06-powerwatt34.jpg",
-            alt: "Dualtron Togo LTD 12Ah - Image 6 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n06-powerwatt34.jpg",
+            alt: "Dualtron Togo Pro 13Ah - Image 6 - PowerWatt34",
         },
         {
             reactKey: 7,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-ltd/dualtron-togo-ltd-n07-powerwatt34.jpg",
-            alt: "Dualtron Togo LTD 12Ah - Image 7 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n07-powerwatt34.jpg",
+            alt: "Dualtron Togo Pro 13Ah - Image 7 - PowerWatt34",
+        },
+        {
+            reactKey: 8,
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n08-powerwatt34.jpg",
+            alt: "Dualtron Togo Pro 13Ah - Image 8 - PowerWatt34",
+        },
+        {
+            reactKey: 9,
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n09-powerwatt34.jpg",
+            alt: "Dualtron Togo Pro 13Ah - Image 9 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronTogoLTD12Ah_img_Data };
+export { dualtronTogoPro13Ah_img_Data };
