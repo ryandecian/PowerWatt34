1cdaa103a092ffdcf40acb54969b516ab2174f07
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-24 12:12:34 +0100

    Création de dualtronTogoPlus12Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-togo-plus-12ah/dualtronTogoPlus12Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-togo-plus-12ah/dualtronTogoPlus12Ah.img.data.ts
@@ -1,56 +1,31 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronTogoPro13Ah_img_Data() {
+function dualtronTogoPlus12Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n01-powerwatt34.jpg",
-            alt: "Dualtron Togo Pro 13,5Ah - Image 1 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-plus/dualtron-togo-plus-n01-powerwatt34.jpg",
+            alt: "Dualtron Togo Plus 12Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n02-powerwatt34.jpg",
-            alt: "Dualtron Togo Pro 13,5Ah - Image 2 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-plus/dualtron-togo-plus-n02-powerwatt34.jpg",
+            alt: "Dualtron Togo Plus 12Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n03-powerwatt34.jpg",
-            alt: "Dualtron Togo Pro 13,5Ah - Image 3 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-plus/dualtron-togo-plus-n03-powerwatt34.jpg",
+            alt: "Dualtron Togo Plus 12Ah - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n04-powerwatt34.jpg",
-            alt: "Dualtron Togo Pro 13,5Ah - Image 4 - PowerWatt34",
-        },
-        {
-            reactKey: 5,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n05-powerwatt34.jpg",
-            alt: "Dualtron Togo Pro 13,5Ah - Image 5 - PowerWatt34",
-        },
-        {
-            reactKey: 6,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n06-powerwatt34.jpg",
-            alt: "Dualtron Togo Pro 13,5Ah - Image 6 - PowerWatt34",
-        },
-        {
-            reactKey: 7,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n07-powerwatt34.jpg",
-            alt: "Dualtron Togo Pro 13,5Ah - Image 7 - PowerWatt34",
-        },
-        {
-            reactKey: 8,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n08-powerwatt34.jpg",
-            alt: "Dualtron Togo Pro 13,5Ah - Image 8 - PowerWatt34",
-        },
-        {
-            reactKey: 9,
-            src: "/images/produits/trottinette/dualtron/dualtron-togo-pro/dualtron-togo-pro-n09-powerwatt34.jpg",
-            alt: "Dualtron Togo Pro 13,5Ah - Image 9 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-togo-plus/dualtron-togo-plus-n04-powerwatt34.jpg",
+            alt: "Dualtron Togo Plus 12Ah - Image 4 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronTogoPro13Ah_img_Data };
+export { dualtronTogoPlus12Ah_img_Data };
