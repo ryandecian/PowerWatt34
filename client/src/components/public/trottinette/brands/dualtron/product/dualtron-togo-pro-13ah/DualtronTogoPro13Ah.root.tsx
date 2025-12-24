beb5049f0bef01754106b4b67b5c2a64b54c8771
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-24 11:46:10 +0100

    Création de DualtronTogoPro13Ah_Root

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-togo-pro-13ah/DualtronTogoPro13Ah.root.tsx
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-togo-pro-13ah/DualtronTogoPro13Ah.root.tsx
@@ -6,18 +6,18 @@ import { CarrouselProduct_Element } from "../../../../../../elements/carrousel/C
 import { TableTrottinette_Element } from "../../../../../../elements/table/TableTrottinette.element";
 
 /* Import des Datas */
-import { dualtronTogoLTD12Ah_Data } from "./dualtronTogoLTD12Ah.data";
-import { dualtronTogoLTD12Ah_img_Data } from "./dualtronTogoLTD12Ah.img.data";
+import { dualtronTogoPro13Ah_Data } from "./dualtronTogoPro13Ah.data";
+import { dualtronTogoPro13Ah_img_Data } from "./dualtronTogoPro13Ah.img.data";
 
 /* Import des Types */
 import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function DualtronTogoLTD12Ah_Root() {
-    const productData: Trottinette_Data_Type = dualtronTogoLTD12Ah_Data();
+function DualtronTogoPro13Ah_Root() {
+    const productData: Trottinette_Data_Type = dualtronTogoPro13Ah_Data();
     return (
-        <section className={`DualtronTogoLTD12Ah_Root ${style.ContainerRootRacine}`}>
+        <section className={`DualtronTogoPro13Ah_Root ${style.ContainerRootRacine}`}>
             <h1 className={style.TitleH1}>{productData.modele}</h1>
-            <CarrouselProduct_Element dataImg={dualtronTogoLTD12Ah_img_Data()} />
+            <CarrouselProduct_Element dataImg={dualtronTogoPro13Ah_img_Data()} />
             <br />
             <p><strong>Prix : {productData.prix}</strong></p>
             <TableTrottinette_Element data={productData} />
@@ -25,4 +25,4 @@ function DualtronTogoLTD12Ah_Root() {
     );
 }
 
-export default DualtronTogoLTD12Ah_Root;
+export default DualtronTogoPro13Ah_Root;
