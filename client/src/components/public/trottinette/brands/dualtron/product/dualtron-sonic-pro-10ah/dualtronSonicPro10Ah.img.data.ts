ae9b6d30d9fc548cd5df17f90913a27027dca0f0
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-24 14:45:47 +0100

    Création de dualtronSonicPro10Ah_img_Data

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-sonic-pro-10ah/dualtronSonicPro10Ah.img.data.ts
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-sonic-pro-10ah/dualtronSonicPro10Ah.img.data.ts
@@ -1,26 +1,41 @@
 /* Import des types */
 import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
 
-function dualtronAchilleus_img_Data() {
+function dualtronSonicPro10Ah_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
-            src: "/images/produits/trottinette/dualtron/dualtron-achilleus/dualtron-achilleus-n01-powerwatt34.jpg",
-            alt: "Dualtron Achilleus - Image 1 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-sonic-pro/dualtron-sonic-pro-n01-powerwatt34.jpg",
+            alt: "Dualtron Sonic Pro 10Ah - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
-            src: "/images/produits/trottinette/dualtron/dualtron-achilleus/dualtron-achilleus-n02-powerwatt34.jpg",
-            alt: "Dualtron Achilleus - Image 2 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-sonic-pro/dualtron-sonic-pro-n02-powerwatt34.jpg",
+            alt: "Dualtron Sonic Pro 10Ah - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
-            src: "/images/produits/trottinette/dualtron/dualtron-achilleus/dualtron-achilleus-n03-powerwatt34.jpg",
-            alt: "Dualtron Achilleus - Image 3 - PowerWatt34",
+            src: "/images/produits/trottinette/dualtron/dualtron-sonic-pro/dualtron-sonic-pro-n03-powerwatt34.jpg",
+            alt: "Dualtron Sonic Pro 10Ah - Image 3 - PowerWatt34",
+        },
+        {
+            reactKey: 4,
+            src: "/images/produits/trottinette/dualtron/dualtron-sonic-pro/dualtron-sonic-pro-n03-powerwatt34.jpg",
+            alt: "Dualtron Sonic Pro 10Ah - Image 4 - PowerWatt34",
+        },
+        {
+            reactKey: 5,
+            src: "/images/produits/trottinette/dualtron/dualtron-sonic-pro/dualtron-sonic-pro-n03-powerwatt34.jpg",
+            alt: "Dualtron Sonic Pro 10Ah - Image 5 - PowerWatt34",
+        },
+        {
+            reactKey: 6,
+            src: "/images/produits/trottinette/dualtron/dualtron-sonic-pro/dualtron-sonic-pro-n03-powerwatt34.jpg",
+            alt: "Dualtron Sonic Pro 10Ah - Image 6 - PowerWatt34",
         },
     ]
 
     return tab;
 }
 
-export { dualtronAchilleus_img_Data };
+export { dualtronSonicPro10Ah_img_Data };
