57fda47731b02c3beacb648532cbcb1a5e2ccda9
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-24 14:47:03 +0100

    Création de DualtronSonicPro10Ah_Root

--- client/src/components/public/trottinette/brands/dualtron/product/dualtron-sonic-pro-10ah/DualtronSonicPro10Ah.root.tsx
+++ client/src/components/public/trottinette/brands/dualtron/product/dualtron-sonic-pro-10ah/DualtronSonicPro10Ah.root.tsx
@@ -6,18 +6,18 @@ import { CarrouselProduct_Element } from "../../../../../../elements/carrousel/C
 import { TableTrottinette_Element } from "../../../../../../elements/table/TableTrottinette.element";
 
 /* Import des Datas */
-import { dualtronAchilleus_Data } from "./dualtronAchilleus.data";
-import { dualtronAchilleus_img_Data } from "./dualtronAchilleus.img.data";
+import { dualtronSonicPro10Ah_Data } from "./dualtronSonicPro10Ah.data";
+import { dualtronSonicPro10Ah_img_Data } from "./dualtronSonicPro10Ah.img.data";
 
 /* Import des Types */
 import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function DualtronAchilleus_Root() {
-    const productData: Trottinette_Data_Type = dualtronAchilleus_Data();
+function DualtronSonicPro10Ah_Root() {
+    const productData: Trottinette_Data_Type = dualtronSonicPro10Ah_Data();
     return (
-        <section className={`DualtronAchilleus_Root ${style.ContainerRootRacine}`}>
+        <section className={`DualtronSonicPro10Ah_Root ${style.ContainerRootRacine}`}>
             <h1 className={style.TitleH1}>{productData.modele}</h1>
-            <CarrouselProduct_Element dataImg={dualtronAchilleus_img_Data()} />
+            <CarrouselProduct_Element dataImg={dualtronSonicPro10Ah_img_Data()} />
             <br />
             <p><strong>Prix : {productData.prix}</strong></p>
             <TableTrottinette_Element data={productData} />
@@ -25,4 +25,4 @@ function DualtronAchilleus_Root() {
     );
 }
 
-export default DualtronAchilleus_Root;
+export default DualtronSonicPro10Ah_Root;
