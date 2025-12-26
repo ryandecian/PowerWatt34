d15b9682f71e5cadb1db1df4aea240f5d01a0ea6
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-26 15:50:05 +0100

    Création de teverunFighter7260RV4Edition2025_img_Data

--- client/src/components/public/trottinette/brands/teverun/product/teverun-fighter-7260r-v4-edition-2025/teverunFighter7260RV4Edition2025.img.data.ts
+++ client/src/components/public/trottinette/brands/teverun/product/teverun-fighter-7260r-v4-edition-2025/teverunFighter7260RV4Edition2025.img.data.ts
@@ -5,23 +5,38 @@ function teverunFighter7260RV4Edition2025_img_Data() {
     const tab: ImageCarousselProduct_Type = [
         {
             reactKey: 1,
-            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-edition-2025-v4/teverun-fighter-7260r-edition-2025-v4-n01-powerwatt34.jpg",
+            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-v4-edition-2025/teverun-fighter-7260r-v4-edition-2025-n01-powerwatt34.jpg",
             alt: "Teverun Fighter 7260R V4 - Édition 2025 - Image 1 - PowerWatt34",
         },
         {
             reactKey: 2,
-            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-edition-2025-v4/teverun-fighter-7260r-edition-2025-v4-n02-powerwatt34.jpg",
+            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-v4-edition-2025/teverun-fighter-7260r-v4-edition-2025-n02-powerwatt34.jpg",
             alt: "Teverun Fighter 7260R V4 - Édition 2025 - Image 2 - PowerWatt34",
         },
         {
             reactKey: 3,
-            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-edition-2025-v4/teverun-fighter-7260r-edition-2025-v4-n03-powerwatt34.jpg",
+            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-v4-edition-2025/teverun-fighter-7260r-v4-edition-2025-n03-powerwatt34.jpg",
             alt: "Teverun Fighter 7260R V4 - Édition 2025 - Image 3 - PowerWatt34",
         },
         {
             reactKey: 4,
-            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-edition-2025-v4/teverun-fighter-7260r-edition-2025-v4-n04-powerwatt34.jpg",
+            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-v4-edition-2025/teverun-fighter-7260r-v4-edition-2025-n04-powerwatt34.jpg",
             alt: "Teverun Fighter 7260R V4 - Édition 2025 - Image 4 - PowerWatt34",
+        },
+        {
+            reactKey: 5,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-v4-edition-2025/teverun-fighter-7260r-v4-edition-2025-n05-powerwatt34.jpg",
+            alt: "Teverun Fighter 7260R V4 - Édition 2025 - Image 5 - PowerWatt34",
+        },
+        {
+            reactKey: 6,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-v4-edition-2025/teverun-fighter-7260r-v4-edition-2025-n06-powerwatt34.jpg",
+            alt: "Teverun Fighter 7260R V4 - Édition 2025 - Image 6 - PowerWatt34",
+        },
+        {
+            reactKey: 7,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-7260r-v4-edition-2025/teverun-fighter-7260r-v4-edition-2025-n07-powerwatt34.jpg",
+            alt: "Teverun Fighter 7260R V4 - Édition 2025 - Image 7 - PowerWatt34",
         },
     ]
 
