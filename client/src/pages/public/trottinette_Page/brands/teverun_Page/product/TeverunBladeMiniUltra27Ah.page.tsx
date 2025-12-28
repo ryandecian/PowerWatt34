94444fcf54eceec82126d8a9bc7712b1c10e9378
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-28 22:44:25 +0100

    Création de TeverunBladeMiniUltra27Ah_Page

--- client/src/pages/public/trottinette_Page/brands/teverun_Page/product/TeverunBladeMiniUltra27Ah.page.tsx
+++ client/src/pages/public/trottinette_Page/brands/teverun_Page/product/TeverunBladeMiniUltra27Ah.page.tsx
@@ -3,22 +3,22 @@ import style from "../../../../../stylePage.module.css";
 
 /* Import des Components */
 import Nav_root_Layout from "../../../../../../components/layout/nav_Layout/Nav.root.layout";
-import TeverunFighterMiniPro25Ah_Root from "../../../../../../components/public/trottinette/brands/teverun/product/teverun-fighter-mini-pro-25ah/TeverunFighterMiniPro25Ah.root";
+import TeverunBladeMiniUltra27Ah_Root from "../../../../../../components/public/trottinette/brands/teverun/product/teverun-blade-mini-ultra-27ah/TeverunBladeMiniUltra27Ah.root";
 import Footer_root_layout from "../../../../../../components/layout/footer_Layout/Footer.root.layout";
 
 /* Import des Hooks */
 import { useScrollToHash_utils_Hook } from "../../../../../../hook/useScrollToHash.utils.hook";
 
-function TeverunFighterMiniPro25Ah_Page() {
+function TeverunBladeMiniUltra27Ah_Page() {
     useScrollToHash_utils_Hook(); /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
 
     return (
-        <section className={`TeverunFighterMiniPro25Ah_Page ${style.Page}`}>
+        <section className={`TeverunBladeMiniUltra27Ah_Page ${style.Page}`}>
             <header id="top" className={style.Header}>
                 <Nav_root_Layout />
             </header>
             <main className={style.Main}>
-                <TeverunFighterMiniPro25Ah_Root />
+                <TeverunBladeMiniUltra27Ah_Root />
             </main>
             <footer className={style.Footer}>
                 <Footer_root_layout />
@@ -27,4 +27,4 @@ function TeverunFighterMiniPro25Ah_Page() {
     );
 }
 
-export default TeverunFighterMiniPro25Ah_Page;
+export default TeverunBladeMiniUltra27Ah_Page;
