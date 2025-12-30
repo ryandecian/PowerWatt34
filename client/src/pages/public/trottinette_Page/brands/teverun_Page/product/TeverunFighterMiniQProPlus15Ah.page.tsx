fe333bb7e15dbfc5f8d3357882bd0285e48da49f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-30 12:54:10 +0100

    Création de TeverunFighterMiniQProPlus15Ah_Page

---
+++ client/src/pages/public/trottinette_Page/brands/teverun_Page/product/TeverunFighterMiniQProPlus15Ah.page.tsx
@@ -0,0 +1,30 @@
+/* Import des modules CSS */
+import style from "../../../../../stylePage.module.css";
+
+/* Import des Components */
+import Nav_root_Layout from "../../../../../../components/layout/nav_Layout/Nav.root.layout";
+import TeverunFighterMiniQProPlus15Ah_Root from "../../../../../../components/public/trottinette/brands/teverun/product/teverun-fighter-mini-q-pro-plus-15ah/TeverunFighterMiniQProPlus15Ah.root";
+import Footer_root_layout from "../../../../../../components/layout/footer_Layout/Footer.root.layout";
+
+/* Import des Hooks */
+import { useScrollToHash_utils_Hook } from "../../../../../../hook/useScrollToHash.utils.hook";
+
+function TeverunFighterMiniQProPlus15Ah_Page() {
+    useScrollToHash_utils_Hook(); /* Utilisation du hook pour gérer le scroll vers les sections avec des IDs */
+
+    return (
+        <section className={`TeverunFighterMiniQProPlus15Ah_Page ${style.Page}`}>
+            <header id="top" className={style.Header}>
+                <Nav_root_Layout />
+            </header>
+            <main className={style.Main}>
+                <TeverunFighterMiniQProPlus15Ah_Root />
+            </main>
+            <footer className={style.Footer}>
+                <Footer_root_layout />
+            </footer>
+        </section>
+    );
+}
+
+export default TeverunFighterMiniQProPlus15Ah_Page;
