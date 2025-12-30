8e81f75374a19101905f67b7766e3d59ae80e8be
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-30 10:12:28 +0100

    Création de TeverunFighterMiniQProPlus15Ah_Root

--- client/src/components/public/trottinette/brands/teverun/product/teverun-fighter-mini-q-pro-plus-15ah/TeverunFighterMiniQProPlus15Ah.root.tsx
+++ client/src/components/public/trottinette/brands/teverun/product/teverun-fighter-mini-q-pro-plus-15ah/TeverunFighterMiniQProPlus15Ah.root.tsx
@@ -6,18 +6,18 @@ import { CarrouselProduct_Element } from "../../../../../../elements/carrousel/C
 import { TableTrottinette_Element } from "../../../../../../elements/table/TableTrottinette.element";
 
 /* Import des Datas */
-import { teverunFighterMini25Ah_Data } from "./teverunFighterMini25Ah.data";
-import { teverunFighterMini25Ah_img_Data } from "./teverunFighterMini25Ah.img.data";
+import { teverunFighterMiniQProPlus15Ah_Data } from "./teverunFighterMiniQProPlus15Ah.data";
+import { teverunFighterMiniQProPlus15Ah_img_Data } from "./teverunFighterMiniQProPlus15Ah.img.data";
 
 /* Import des Types */
 import type { Trottinette_Data_Type } from "../../../../../../../types/produits/trottinette/trottinette.data.type";
 
-function TeverunFighterMini25Ah_Root() {
-    const productData: Trottinette_Data_Type = teverunFighterMini25Ah_Data();
+function TeverunFighterMiniQProPlus15Ah_Root() {
+    const productData: Trottinette_Data_Type = teverunFighterMiniQProPlus15Ah_Data();
     return (
-        <section className={`TeverunFighterMini25Ah_Root ${style.ContainerRootRacine}`}>
+        <section className={`TeverunFighterMiniQProPlus15Ah_Root ${style.ContainerRootRacine}`}>
             <h1 className={style.TitleH1}>{productData.modele}</h1>
-            <CarrouselProduct_Element dataImg={teverunFighterMini25Ah_img_Data()} />
+            <CarrouselProduct_Element dataImg={teverunFighterMiniQProPlus15Ah_img_Data()} />
             <br />
             <p><strong>Prix : {productData.prix}</strong></p>
             <TableTrottinette_Element data={productData} />
@@ -25,4 +25,4 @@ function TeverunFighterMini25Ah_Root() {
     );
 }
 
-export default TeverunFighterMini25Ah_Root;
+export default TeverunFighterMiniQProPlus15Ah_Root;
