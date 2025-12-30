8169a13aa4b1735b7ac00e3e2c7ac1143637f2e8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-30 09:50:18 +0100

    Création de teverunFighterMiniQProPlus15Ah_Data

---
+++ client/src/components/public/trottinette/brands/teverun/product/teverun-fighter-mini-q-pro-plus-15ah/teverunFighterMiniQProPlus15Ah.img.data.ts
@@ -0,0 +1,76 @@
+/* Import des types */
+import type { ImageCarousselProduct_Type } from "../../../../../../../types/produits/imageCarousselProduct.type";
+
+function teverunFighterMini25Ah_img_Data() {
+    const tab: ImageCarousselProduct_Type = [
+        {
+            reactKey: 1,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n01-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 1 - PowerWatt34",
+        },
+        {
+            reactKey: 2,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n02-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 2 - PowerWatt34",
+        },
+        {
+            reactKey: 3,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n03-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 3 - PowerWatt34",
+        },
+        {
+            reactKey: 4,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n04-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 4 - PowerWatt34",
+        },
+        {
+            reactKey: 5,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n05-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 5 - PowerWatt34",
+        },
+        {
+            reactKey: 6,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n06-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 6 - PowerWatt34",
+        },
+        {
+            reactKey: 7,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n07-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 7 - PowerWatt34",
+        },
+        {
+            reactKey: 8,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n08-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 8 - PowerWatt34",
+        },
+        {
+            reactKey: 9,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n09-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 9 - PowerWatt34",
+        },
+        {
+            reactKey: 10,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n10-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 10 - PowerWatt34",
+        },
+        {
+            reactKey: 11,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n11-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 11 - PowerWatt34",
+        },
+        {
+            reactKey: 12,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n12-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 12 - PowerWatt34",
+        },
+        {
+            reactKey: 13,
+            src: "/images/produits/trottinette/teverun/teverun-fighter-mini/teverun-fighter-mini-n13-powerwatt34.jpg",
+            alt: "Teverun Fighter Mini - 25Ah - Image 13 - PowerWatt34",
+        },
+    ]
+
+    return tab;
+}
+
+export { teverunFighterMini25Ah_img_Data };
