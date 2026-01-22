955fabff4080e52a8dedc5405d324db3ce6c795a
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-01-22 16:56:43 +0100

    Mise a jour de dataSEO_Home_data_SEO avec ses types

--- client/src/seo/home_SEO/dataSEO_Home.data.seo.ts
+++ client/src/seo/home_SEO/dataSEO_Home.data.seo.ts
@@ -13,25 +13,8 @@ function dataSEO_Home_data_SEO(): DataSEOTargetOne_Type {
         url: `${import.meta.env.VITE_DOMAIN_CLIENT}`, /*URL de la page*/
         position: 1, /* Clé pour Breadcrumb : Position UX de la page lors de la navigation dans le site */
         name_Breadcrumb: "Accueil", /* Le libellé du lien dans le fil d’Ariane. Exemple : Portfolio ou Portrait Duo */
-        name_Service: "Accueil - PowerWatt34", /* Nom du service, utilisé dans le JSON-LD Service */
+        name_PageGoogle: "Accueil - PowerWatt34", /* Nom de la page affichée dans les résultats Google */
         twitterUrlImg: "", /* Lien URL de l'image */
-        keywords: {
-            1: "PowerWatt34", /* 1 mot clés */
-            2: "Atelier réparation trottinette", /* 2 mots clés */
-            3: "Réparation trottinette électrique Lattes", /* 3 mots clés */
-            5: "Réparation scooter électrique", /* 4 mots clés */
-            4: "Réparation vélo électrique", /* 5 mots clés */
-            6: "Atelier trottinette Montpellier", /* 6 mots clés */
-            7: "Diagnostic trottinette électrique", /* 7 mots clés */
-            8: "Réparation batterie trottinette", /* 8 mots clés */
-            9: "Réparation moteur trottinette", /* 9 mots clés */
-            10: "Entretien trottinette électrique", /* 10 mots clés */
-            11: "Révision scooter électrique", /* 11 mots clés */
-            12: "Atelier 2 roues électriques", /* 12 mots clés */
-            13: "Réparation trottinette Montpellier", /* 13 mots clés */
-            14: "Réparation scooter Lattes", /* 14 mots clés */
-            15: "Réparation vélo électrique Montpellier", /* 15 mots clés */
-        }, /* 10 a 15 mots max */
         type : {
             website: "website", /*(Valeur par défaut) indique qu'il s'agit d'un site web classique.*/
             article: "article", /*Pour des articles de blog ou du contenu éditorial.*/
