6d639cd6370ffeb80694db519558aafb443fe372
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-01-23 17:34:23 +0100

    Création de jsonString_Utils

--- client/src/seo/trottinette_SEO/JSON-LD_Breadcrumb_Trottinette.schema.seo.ts
+++ client/src/seo/trottinette_SEO/JSON-LD_Breadcrumb_Trottinette.schema.seo.ts
@@ -2,35 +2,37 @@
 import { dataSEO_Root_data_SEO } from "../dataSEO_Root.data.seo";
 import { dataSEO_Trottinette_data_SEO } from "./dataSEO_Trottinette.data.seo";
 
+/* Import des composants SEO */
+import { JSON_LD_Breadcrumb_Home_schema_SEO } from "../home_SEO/JSON-LD_Breadcrumb_Home.schema.seo";
+
 /* Import des Types */
 import type { DataSEORoot_Type } from "../../types/seo/dataSEORoot.type";
 import type { DataSEOTargetOne_Type } from "../../types/seo/dataSEOTargetOne.type";
 
-function JSON_LD_Breadcrumb_Trottinette_schema_SEO() : string {
-    const dataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO(); /* Récupération des données SEO */
-    const dataSEO_Trottinette: DataSEOTargetOne_Type = dataSEO_Trottinette_data_SEO(); /* Récupération des données SEO de la page */
+function JSON_LD_Breadcrumb_Trottinette_schema_SEO(): string {
+    const dataSEORoot: DataSEORoot_Type = dataSEO_Root_data_SEO();
+    const dataSEO_Trottinette: DataSEOTargetOne_Type = dataSEO_Trottinette_data_SEO();
 
     const JSON_LD = JSON.stringify({
         "@context": dataSEORoot["@context"],
         "@type": dataSEORoot["@type"].BreadcrumbList,
         "itemListElement": [
+            JSON_LD_Breadcrumb_Home_schema_SEO().itemListElement[0],
             {
                 "@type": "ListItem",
                 "position": dataSEO_Trottinette.position,
-                "name": dataSEO_Trottinette.name_Breadcrumb, /* Le libellé du lien dans le fil d’Ariane */
+                "name": dataSEO_Trottinette.name_Breadcrumb,
                 "item": {
                     "@type": dataSEORoot["@type"].WebPage,
                     "@id": dataSEO_Trottinette.id_Service,
                     "url": dataSEO_Trottinette.url,
-                    "name": dataSEO_Trottinette.name_Breadcrumb
-                }
+                    "name": dataSEO_Trottinette.name_Breadcrumb,
+                },
             },
         ],
     });
 
-    return (
-        JSON_LD
-    )
+    return JSON_LD;
 }
 
 export { JSON_LD_Breadcrumb_Trottinette_schema_SEO };
